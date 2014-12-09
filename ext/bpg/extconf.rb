23615d762c08ea456dd485a8938ae8ac7d948101
tbraun89 <tbraun@tnt-web-solutions.de>
2014-12-09 13:50:37 +0100

    added basic native extension

--- ext/bpg/extconf.rb
+++ ext/bpg/extconf.rb
@@ -1,5 +1,16 @@
 require 'mkmf'
 
-ext_name = 'bpg'
+ext_name    = 'bpg'
+
+LIBDIR      = Config::CONFIG['libdir']
+INCLUDEDIR  = Config::CONFIG['includedir']
+
+HEADER_DIRS = [
+    File.join(File.dirname(__FILE__), 'libbpg'),
+
+    INCLUDEDIR
+]
+
+dir_config(ext_name, HEADER_DIRS)
 
 create_makefile(ext_name)
