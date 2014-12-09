c06eb20284c9668d3d42cf53cc44c8f062262c8b
tbraun89 <tbraun@tnt-web-solutions.de>
2014-12-10 00:57:04 +0100

    start of the HeicFile

--- lib/bpg/file_format.rb
+++ lib/bpg/file_format.rb
@@ -1,7 +1,17 @@
+require 'bindata'
+
 module Bpg
   module FileFormat
-    class HeicFile < BinData
+    class HeicFile < BinData::Record
+      sbit :file_magic,             nbits: 32
+      sbit :pixel_format,           nbits: 3
+      sbit :alpha_present_flag,     nbits: 1
+      sbit :bit_depth_minus_8,      nbits: 4
+      sbit :color_space,            nbits: 4
+      sbit :extension_present_flag, nbits: 1
+      sbit :reserved_zeros,         nbits: 3
 
+      # TODO
     end
   end
 end
